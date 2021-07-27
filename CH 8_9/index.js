fd441b800908c11c7b0a0947b87e17835d838a51
Farhan-PK <farhanwebsolution@gmail.com>
2021-07-27 17:56:58 +0500

    Practice Chapter 8_9

---
+++ CH 8_9/index.js
@@ -0,0 +1,4 @@
+var firstName = prompt("what is your First Name?","");
+var lastName = prompt("what is your Last Name?","");
+var age = prompt("what is your Age?","");
+alert("Hello "+ firstName+" "+lastName+","+"You are "+ age+" years old.");
