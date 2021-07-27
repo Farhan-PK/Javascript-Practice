2f69cadebfecc458870cc2eac9d551ba2a012418
Farhan-PK <farhanwebsolution@gmail.com>
2021-07-27 19:27:27 +0500

    Practice chapter 10 to 14...with 2 question

--- CH 10_14/index.js
+++ CH 10_14/index.js
@@ -1,10 +1,11 @@
-// Java-script assignments chapter 10 to 14
+// // Java-script assignments chapter 10 to 14
 
-// Make a Calculator
-// input a value from user for example num1 is 7
-// input a second value from user for example num2 is 3
-// input a third value from user for example sign is "+" or "-" or "*" or "/" or "%"
-// print operation in alert() 7-3 =4, or 7*3 =21, or 7/3 =2.3, or 7+3 =10 or 7%3 =1
+// // Make a Calculator
+// // input a value from user for example num1 is 7
+// // input a second value from user for example num2 is 3
+// // input a third value from user for example sign is "+" or "-" or "*" or "/" or "%"
+// // print operation in alert() 7-3 =4, or 7*3 =21, or 7/3 =2.3, or 7+3 =10 or 7%3 =1
+
 var num1 =Number(prompt("Enter First Number",""));
 var num2 =Number(prompt("Enter second Number",""));
 var operator =prompt("Enter Operator","");
@@ -36,4 +37,29 @@ else if(operator ==='%')
 else
 {
     alert("Your Chose wrong operator");
+}
+
+/* ************************************************ */
+
+// Second Assignment - Make a Percentage Calculator
+// Get the 5-course number from the userfor example course1 is 70 var totalMarks = 500
+// Calculate the total percentage formula: obtainedMarks * 100/ totalMarks
+// print output in alert.
+
+var course1 =Number(prompt("enter Your Math marks",""));
+var course2 =Number(prompt("enter Your Urdu marks",""));
+var course3 =Number(prompt("enter Your Computer marks",""));
+var course4 =Number(prompt("enter Your Physics marks",""));
+var course5 =Number(prompt("enter Your Chemisrt marks",""));
+var sum = course1+course2+course3+course4+course5;
+var totalMarks = 500;
+var percent= (sum*100)/totalMarks;
+console.log(percent);
+if (percent>60)
+{
+    alert("Congress you are pass");
+}
+else
+{
+alert("sorry you are fail");
 }
