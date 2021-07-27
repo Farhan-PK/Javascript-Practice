fd7d42a239884377e40559ce26daf2840111d4b1
Farhan-PK <farhanwebsolution@gmail.com>
2021-07-27 17:19:53 +0500

    Practice Chapter 4_5_6_7

---
+++ CH 4_5_6_7/index.js
@@ -0,0 +1,11 @@
+// var a = 1;
+// var b = a++; //at this line b = 2 because of post increament(b= a+1) 
+// alert("A ="+ a); //1
+// alert("B ="+ b);//2
+// var c =2;
+// var d= ++c;//at this line d = 3 because of pre increament(d= c+1)
+// alert("C = "+ c)// here C= 3;
+// alert("D = "+ d)// here D= 3;
+// console.log("3"+ 4);
+// console.log(3+ 4);
+console.log("3" - 4);
