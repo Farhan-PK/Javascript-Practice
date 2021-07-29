23e4e66e81e409ade9246dfc9549eed29680f9ee
Farhan-PK <farhanwebsolution@gmail.com>
2021-07-30 00:11:30 +0500

    Practice for loop and splice() and push() minor changes

--- CH 18_19_20/index.js
+++ CH 18_19_20/index.js
@@ -1,9 +1,10 @@
 var cities = [];
-for(var i=0;i < 3;i++)
+for(var i=0;i < 5;i++)
 {
-    cities.push(prompt("Enter Cites Name: ",""));
+    var a= prompt("Enter Cites Name: ","");
+    cities.push(a);
 }
-for(var i=0;i < 3;i++)
+for(var i=0;i < 5;i++)
 {
     cities[i]=prompt(cities[i],cities[i]);
     cities.splice(i,1,cities[i])
