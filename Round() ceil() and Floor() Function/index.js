d127beb911393669292f56eac0c276ab8fbc7eda
Farhan-PK <farhanwebsolution@gmail.com>
2021-08-01 16:52:05 +0500

    Add commit of funtion

--- Round() ceil() and Floor() Function/index.js
+++ Round() ceil() and Floor() Function/index.js
@@ -1,12 +1,12 @@
 var num =23.6;
 alert(num);
-num =Math.round(num);
+num =Math.round(num); //it round the number to the nearset point
 alert(num);
 var num1 =23.6;
 alert(num1);
-num1 =Math.ceil(num1);
+num1 =Math.ceil(num1);//it forcefully round the number to upper point 23.2=24
 alert(num1);
 var num2 =23.6;
 alert(num2);
-num2 =Math.floor(num2);
+num2 =Math.floor(num2);//it forcefully round the number to lowest point23.8=23
 alert(num2);
