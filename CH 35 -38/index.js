9e9456ea191a06ffb16da27a0e2f269fc53866d4
Farhan-PK <farhanwebsolution@gmail.com>
2021-08-03 23:07:47 +0500

    Factorial of number calculator

--- CH 35 -38/index.js
+++ CH 35 -38/index.js
@@ -1,18 +1,45 @@
-// input value from user in String var inputValue="cloUd naTive computinG"
-// Convert every first letter of the content in Capitalize case. inputValue="Cloud Native Computing" use Function
-// print value in console.log() console.log(inputValue) | check result in browser console
+// // input value from user in String var inputValue="cloUd naTive computinG"
+// // Convert every first letter of the content in Capitalize case. inputValue="Cloud Native Computing" use Function
+// // print value in console.log() console.log(inputValue) | check result in browser console
 
-var mes = prompt("Enter Your Message: ",);
+// var mes = prompt("Enter Your Message: ",);
 
-function toCapital(message) {
+// function toCapital(message) {
 
-    var arr = message.split(" ");
-    for (var i=0;i<arr.length;i++) {
-        arr[i]=arr[i].charAt(0).toUpperCase()+arr[i].slice(1);
+//     var arr = message.split(" ");
+//     for (var i=0;i<arr.length;i++) {
+//         arr[i]=arr[i].charAt(0).toUpperCase()+arr[i].slice(1);
+
+//     }
+//     return message=arr.join(" ");
+    
+// }
+// mes=toCapital(mes);
+// console.log(mes);
+
+// // User input value in Number through prompt Example value=5
+// // Factorialize a Number use Function
+// // print value in console.log() console.log(value); the result is 120
+
+var num=prompt("Enter A number for factorial: ",);
+num= Number(num);
+var fact=1;
+function factorial(num) {
+    if(num===0)
+    {
+        console.log("Factorial of 0 is is equal to 1");
+    }
+    else if(num<0)
+    {
+        console.log("Factorial of -ive is not posible");
 
     }
-    return message=arr.join(" ");
-    
+    else{
+        for(var i=1;i<=num;i++)
+    {
+        fact=fact*i;
+    }
+    }
+    console.log("Factorial of "+num+" = to "+fact);
 }
-mes=toCapital(mes);
-console.log(mes);
+factorial(num);
