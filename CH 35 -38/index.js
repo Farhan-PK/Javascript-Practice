2e8a96935125bef012eaa5b1420c36da0266af80
Farhan-PK <farhanwebsolution@gmail.com>
2021-08-03 23:10:11 +0500

    Factorial of number calculator

--- CH 35 -38/index.js
+++ CH 35 -38/index.js
@@ -1,25 +1,26 @@
-// // input value from user in String var inputValue="cloUd naTive computinG"
-// // Convert every first letter of the content in Capitalize case. inputValue="Cloud Native Computing" use Function
-// // print value in console.log() console.log(inputValue) | check result in browser console
+// input value from user in String var inputValue="cloUd naTive computinG"
+// Convert every first letter of the content in Capitalize case. inputValue="Cloud Native Computing" use Function
+// print value in console.log() console.log(inputValue) | check result in browser console
 
-// var mes = prompt("Enter Your Message: ",);
+var mes = prompt("Enter Your Message: ",);
 
-// function toCapital(message) {
+function toCapital(message) {
 
-//     var arr = message.split(" ");
-//     for (var i=0;i<arr.length;i++) {
-//         arr[i]=arr[i].charAt(0).toUpperCase()+arr[i].slice(1);
+    var arr = message.split(" ");
+    for (var i=0;i<arr.length;i++) {
+        arr[i]=arr[i].charAt(0).toUpperCase()+arr[i].slice(1);
 
-//     }
-//     return message=arr.join(" ");
+    }
+    return message=arr.join(" ");
     
-// }
-// mes=toCapital(mes);
-// console.log(mes);
+}
+mes=toCapital(mes);
+console.log(mes);
 
-// // User input value in Number through prompt Example value=5
-// // Factorialize a Number use Function
-// // print value in console.log() console.log(value); the result is 120
+// ********************************************Question 2****************************************************
+// User input value in Number through prompt Example value=5
+// Factorialize a Number use Function
+// print value in console.log() console.log(value); the result is 120
 
 var num=prompt("Enter A number for factorial: ",);
 num= Number(num);
