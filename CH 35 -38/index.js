3588c508582fb6b12a78321a054e618274cdd4be
Farhan-PK <farhanwebsolution@gmail.com>
2021-08-03 23:17:42 +0500

    minor bug fix

--- CH 35 -38/index.js
+++ CH 35 -38/index.js
@@ -8,7 +8,7 @@ function toCapital(message) {
 
     var arr = message.split(" ");
     for (var i=0;i<arr.length;i++) {
-        arr[i]=arr[i].charAt(0).toUpperCase()+arr[i].slice(1);
+        arr[i]=arr[i].charAt(0).toUpperCase()+arr[i].slice(1).toLowerCase();
 
     }
     return message=arr.join(" ");
